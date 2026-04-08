d8f985f9029f31ed12a95326ae98d05da7b51688
Shahid <drshahidqamar@gmail.com>
2026-04-07 21:07:18 -0400

    debug twilio credentials v2

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ if (!accountSid || !authToken) {
   console.error('TWILIO_AUTH_TOKEN:', authToken ? 'SET' : 'MISSING');
 }
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken  = process.env.TWILIO_AUTH_TOKEN;
+console.log('TWILIO_ACCOUNT_SID:', accountSid ? 'SET' : 'MISSING');
+console.log('TWILIO_AUTH_TOKEN:', authToken ? 'SET' : 'MISSING');
 const client = twilio(accountSid, authToken);
 
 app.use(express.urlencoded({ extended: false }));
