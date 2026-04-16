2201fa74640040b883d39c75610d1b81abd44d52
Shahid <drshahidqamar@gmail.com>
2026-04-16 15:02:19 -0400

    Level2 Eng Urdu Math Added

--- index.js
+++ index.js
@@ -812,6 +812,7 @@ app.get('/import', (req, res) => res.sendFile(path.join(__dirname, 'import.html'
 app.get('/image-portal', (req, res) => res.sendFile(path.join(__dirname, 'image-portal.html')));
 app.get('/pdf-portal', (req, res) => res.sendFile(path.join(__dirname, 'takmil-pdf-portal.html')));
 app.get('/pdf-portal', (req, res) => res.sendFile(path.join(__dirname, 'takmil-pdf-portal-v2.html')));
+app.get('/level2', (req,res) => res.sendFile(path.join(__dirname,'takmil-level2-portal.html')));
 // ══════════════════════════════════════════════════════════════════
 // ADD THESE TWO BLOCKS TO index.js
 // Paste BLOCK A anywhere after the existing app.get('/import') line
